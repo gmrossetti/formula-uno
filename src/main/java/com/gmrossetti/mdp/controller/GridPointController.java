337a76e0cb4293e3f1b876924cf1f161a4c6f4d5
Gian <gmrrossetti@yahoo.com>
2025-01-31 21:43:13 +0100

    Refactor | GridPointController

--- src/main/java/com/gmrossetti/mdp/controller/GridPointController.java
+++ src/main/java/com/gmrossetti/mdp/controller/GridPointController.java
@@ -8,12 +8,6 @@ public class GridPointController {
     private final GridPoint model;
     private final GridPointView view;
 
-    public GridPointController(int x, int y, GridPoint.GridPointType type){
-        this(new GridPoint(x, y, type));
-    }
-    public GridPointController(int x, int y, GridPoint.GridPointType type, PlayerController occupiedBy){
-        this(new GridPoint(x, y, type, occupiedBy));
-    }
     public GridPointController(GridPoint model){
         this.model = new GridPoint(model);
         this.view = new GridPointView(this.model);
