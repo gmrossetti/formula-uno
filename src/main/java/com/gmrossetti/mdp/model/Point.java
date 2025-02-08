64c1b0948567ab70eaf05323540fa8a0443c11af
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-08 15:16:06 +0100

    Add | utils intersection methods in Point class

--- src/main/java/com/gmrossetti/mdp/model/Point.java
+++ src/main/java/com/gmrossetti/mdp/model/Point.java
@@ -1,6 +1,9 @@
 package com.gmrossetti.mdp.model;
 
+import javafx.util.Pair;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Point {
     public final int x;
@@ -48,6 +51,49 @@ public class Point {
 
         return new Point(newX, newY);
     }
+    public static Pair<Double,Double> calcIntersectionCoords(Pair<Point,Point> lineA, Pair<Point,Point> lineB) {
+        double x1 = lineA.getKey().x;
+        double y1 = lineA.getKey().y;
+        double x2 = lineA.getValue().x;
+        double y2 = lineA.getValue().y;
+
+        double x3 = lineB.getKey().x;
+        double y3 = lineB.getKey().y;
+        double x4 = lineB.getValue().x;
+        double y4 = lineB.getValue().y;
+
+        // Calcola i parametri della retta 1 (linea 1)
+        double denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+        if (denom == 0) {
+            // Le linee sono parallele, non hanno intersezione
+            return null; // oppure gestire l'errore come preferisci
+        }
+
+        // Calcola il punto di intersezione
+        double intersectX = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom;
+        double intersectY = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom;
+
+        return new Pair<>(intersectX, intersectY);
+    }
+
+    public static Set<Point> findClosestIntegerPoints(Pair<Double,Double> coords){
+        int floorX = (int) Math.floor(coords.getKey());
+        int ceilX = (int) Math.ceil(coords.getKey());
+
+        int floorY = (int) Math.floor(coords.getValue());
+        int ceilY = (int) Math.ceil(coords.getValue());
+
+        Set<Point> closestIntegerPoints = new HashSet<>();
+
+        closestIntegerPoints.add(new Point(floorX,floorY));
+        closestIntegerPoints.add(new Point(floorX,ceilY));
+        closestIntegerPoints.add(new Point(ceilX,floorY));
+        closestIntegerPoints.add(new Point(ceilX,ceilY));
+
+        return closestIntegerPoints;
+    }
+
     @Override
     public boolean equals(Object other) {
         if (this == other) return true;
