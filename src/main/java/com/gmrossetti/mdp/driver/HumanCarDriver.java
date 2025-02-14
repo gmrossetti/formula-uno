5510a4d2ac12139ac3eb4617e0c665614ac86949
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-14 17:37:24 +0100

    Add | HumanCarDriver class

---
+++ src/main/java/com/gmrossetti/mdp/driver/HumanCarDriver.java
@@ -0,0 +1,9 @@
+package com.gmrossetti.mdp.driver;
+
+import com.gmrossetti.mdp.actor.Car;
+
+public class HumanCarDriver extends CarDriver {
+    public HumanCarDriver(Car car) {
+        super(car);
+    }
+}
