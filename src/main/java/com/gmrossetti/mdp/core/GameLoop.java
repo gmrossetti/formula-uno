f8c47583dbc85d12f14fdae5d6cc6e831fab7514
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-14 18:35:57 +0100

    Refactoring

--- src/main/java/com/gmrossetti/mdp/core/GameLoop.java
+++ src/main/java/com/gmrossetti/mdp/core/GameLoop.java
@@ -1,11 +1,16 @@
 package com.gmrossetti.mdp.core;
 
+import com.gmrossetti.mdp.driver.HumanCarDriver;
 import javafx.animation.AnimationTimer;
 
 public class GameLoop extends AnimationTimer {
+    private final GameState gameState;
+    private final GameLogic gameLogic;
     private final Renderer renderer;
 
-    public GameLoop(Renderer renderer) {
+    public GameLoop(GameState gameState, GameLogic gameLogic, Renderer renderer) {
+        this.gameState = gameState;
+        this.gameLogic = gameLogic;
         this.renderer = renderer;
     }
 
@@ -13,7 +18,15 @@ public class GameLoop extends AnimationTimer {
     public void handle(long l) {
         // aggiornamento stato tramite logica
 
+//        gameLogic.
 
+        HumanCarDriver humanCarDriver = gameState.getHumanCarDriver();
+
+        if(humanCarDriver != null){
+            if(InputHandler.getInstance().hasMove()){
+                humanCarDriver.makeMove(InputHandler.getInstance().popMove());
+            }
+        }
 
         renderer.update();
     }
