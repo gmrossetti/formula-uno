359ff30388fd8a90328b792c480471f0319c6573
Gian <gmrrossetti@yahoo.com>
2025-02-16 20:49:39 +0100

    Fix | RaceView overflow issues

--- src/main/java/com/gmrossetti/mdp/view/RaceView.java
+++ src/main/java/com/gmrossetti/mdp/view/RaceView.java
@@ -3,16 +3,27 @@ package com.gmrossetti.mdp.view;
 import com.gmrossetti.mdp.core.GameState;
 import com.gmrossetti.mdp.driver.CarDriver;
 import com.gmrossetti.mdp.actor.Car;
-import javafx.scene.Group;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 
-public class RaceView extends Group {
+public class RaceView extends StackPane {
     private CircuitView circuitView;
     private ArrayList<CarView> carViews = new ArrayList<>();
 
     public RaceView(GameState gameState) {
         this.circuitView = new CircuitView(gameState.getCircuit());
+
+        Rectangle clip = new Rectangle();
+
+        // Usiamo le property per aggiornare il clip dinamicamente
+        clip.widthProperty().bind(this.widthProperty());
+        clip.heightProperty().bind(this.heightProperty());
+
+        this.setClip(clip); // Nasconde gli elementi fuori dal limite
+
+        this.setStyle("-fx-padding: 20px;");
 
         for (CarDriver carDriver:
                 gameState.getCarDrivers()) {
