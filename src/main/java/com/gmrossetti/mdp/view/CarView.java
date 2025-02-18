f2786ba68a4d550c764045f0f64dc13cd0e9753e
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-19 00:50:54 +0100

    Fix | trail lines after reset

--- src/main/java/com/gmrossetti/mdp/view/CarView.java
+++ src/main/java/com/gmrossetti/mdp/view/CarView.java
@@ -2,6 +2,7 @@ package com.gmrossetti.mdp.view;
 
 import com.gmrossetti.mdp.entity.GridPoint;
 import com.gmrossetti.mdp.actor.Car;
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -16,14 +17,13 @@ public class CarView extends Pane {
     public final int RECT_HEIGHT = 13;
 
     private final Shape shape;
-
     private final Rectangle pivotRect;
+    private final Group trailSegments;
 
     public CarView(Car car){
         shape = new Rectangle(-RECT_WIDTH/2.d,-RECT_HEIGHT/2.d,RECT_WIDTH,RECT_HEIGHT);
         shape.setFill(Color.YELLOW);
         shape.setRotate(45);
-
 
         pivotRect = new Rectangle(-RECT_WIDTH/2.d,-RECT_HEIGHT/2.d,RECT_WIDTH,RECT_HEIGHT);
         Color color = new Color(Color.YELLOW.getRed(), Color.YELLOW.getGreen(), Color.YELLOW.getBlue(), 0.6);
@@ -31,34 +31,45 @@ public class CarView extends Pane {
         pivotRect.setFill(color);
         pivotRect.setRotate(45);
 
+        trailSegments = new Group();
 
         update(car);
 
         this.getChildren().add(shape);
 
         this.getChildren().add(pivotRect);
+
+        this.getChildren().add(trailSegments);
     }
 
     public void update(Car car){
         shape.setTranslateX(car.getPosition().x * SPACING);
         shape.setTranslateY(car.getPosition().y * SPACING);
 
-        addTrailLine(car);
+        updateTrailSegments(car);
         updatePivotPoint(car);
     }
 
-    private void addTrailLine(Car car){
+    private void updateTrailSegments(Car car){
+
+        trailSegments.getChildren().clear();
+
         final List<GridPoint> carTrail = car.getTrail();
 
         if(carTrail.size() < 2) return;
 
-        GridPoint gpA = carTrail.get(carTrail.size() - 2);
-        GridPoint gpB = carTrail.get(carTrail.size() - 1);
+        for (int i = 0; i < carTrail.size(); i++) {
+            if(i == 0) continue;
 
-        Line trailSegment = new Line(gpA.x * SPACING, gpA.y * SPACING, gpB.x * SPACING, gpB.y * SPACING);
-        trailSegment.setStroke(Color.YELLOW);
+            GridPoint gpA = carTrail.get(i - 1);
+            GridPoint gpB = carTrail.get(i);
 
-        this.getChildren().add(trailSegment);
+            Line trailSegment = new Line(gpA.x * SPACING, gpA.y * SPACING, gpB.x * SPACING, gpB.y * SPACING);
+            trailSegment.setStroke(Color.YELLOW);
+            trailSegment.setStrokeWidth(2);
+
+            trailSegments.getChildren().add(trailSegment);
+        }
     }
 
     private void updatePivotPoint(Car car){
