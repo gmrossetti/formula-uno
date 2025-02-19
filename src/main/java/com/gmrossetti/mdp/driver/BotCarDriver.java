971a3b32753a59bfc52e6586106f18a57fe2836c
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-19 17:40:50 +0100

    Add | Bot random move

--- src/main/java/com/gmrossetti/mdp/driver/BotCarDriver.java
+++ src/main/java/com/gmrossetti/mdp/driver/BotCarDriver.java
@@ -3,14 +3,18 @@ package com.gmrossetti.mdp.driver;
 import com.gmrossetti.mdp.actor.Car;
 import com.gmrossetti.mdp.entity.GridLine;
 
+import java.util.Random;
+
 public class BotCarDriver extends CarDriver{
     public BotCarDriver(Car car) {
         super(car);
     }
 
     public final GridLine makeMove(){
-        // TODO: add move decision logic
-        Move move = Move.BR;
+        // TODO: add move decision logic (now is random)
+        // Seleziona a caso una mossa
+        Random random = new Random();
+        Move move = Move.values()[random.nextInt(Move.values().length)];
 
         return super.processMove(move);
     }
