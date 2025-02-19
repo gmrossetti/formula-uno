107bf4c8319b5920fdc15c2ac599d6c2babd2209
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-19 16:16:30 +0100

    Add & Refactor | Bot and Human nextStep()

--- src/main/java/com/gmrossetti/mdp/driver/HumanCarDriver.java
+++ src/main/java/com/gmrossetti/mdp/driver/HumanCarDriver.java
@@ -1,9 +1,14 @@
 package com.gmrossetti.mdp.driver;
 
 import com.gmrossetti.mdp.actor.Car;
+import com.gmrossetti.mdp.entity.GridLine;
 
 public class HumanCarDriver extends CarDriver {
     public HumanCarDriver(Car car) {
         super(car);
+    }
+
+    public final GridLine makeMove(Move move){
+        return super.processMove(move);
     }
 }
