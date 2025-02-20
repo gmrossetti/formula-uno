384bda108a619bad414d6edeae1460495458f0a9
Gian <gmrrossetti@yahoo.com>
2025-02-20 23:36:18 +0100

    Add | gridPointsInRange()

--- src/main/java/com/gmrossetti/mdp/entity/GridPoint.java
+++ src/main/java/com/gmrossetti/mdp/entity/GridPoint.java
@@ -1,5 +1,8 @@
 package com.gmrossetti.mdp.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GridPoint extends AbstractPoint<Integer> {
     public GridPoint(int x, int y) {
         super(Integer.valueOf(x), Integer.valueOf(y));
@@ -24,5 +27,24 @@ public class GridPoint extends AbstractPoint<Integer> {
         int newY = this.y - point2sub.y;
 
         return new GridPoint(newX, newY);
+    }
+
+    public Set<GridPoint> gridPointsInRange(int radius) {
+        Set<GridPoint> pointsInradius = new HashSet<>();
+
+        final int x0 = this.x;
+        final int y0 = this.y;
+
+        // Esplora i punti nel cerchio di radius R
+        for (int x = x0 - radius; x <= x0 + radius; x++) {
+            for (int y = y0 - radius; y <= y0 + radius; y++) {
+                // Controlla se il punto (x, y) è all'interno del cerchio
+                if ((Math.pow(x - x0, 2) + Math.pow(y - y0, 2)) <= Math.pow(radius, 2)) {
+                    pointsInradius.add(new GridPoint(x, y));
+                }
+            }
+        }
+
+        return pointsInradius;
     }
 }
