ba9a90230ed2a048a6a2a90dc1fbfd1eca4b9365
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-22 21:34:09 +0100

    Fix | module-info after refactor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,17 +3,14 @@ module com.gmrossetti.mdp {
     requires javafx.fxml;
     requires java.desktop;
 
-
     opens com.gmrossetti.mdp to javafx.fxml;
     exports com.gmrossetti.mdp;
     exports com.gmrossetti.mdp.actor;
     opens com.gmrossetti.mdp.actor to javafx.fxml;
     exports com.gmrossetti.mdp.level;
     opens com.gmrossetti.mdp.level to javafx.fxml;
-    exports com.gmrossetti.mdp.entity;
-    opens com.gmrossetti.mdp.entity to javafx.fxml;
-    exports com.gmrossetti.mdp.entity.waypoint;
-    opens com.gmrossetti.mdp.entity.waypoint to javafx.fxml;
     exports com.gmrossetti.mdp.entity.cartesian;
     opens com.gmrossetti.mdp.entity.cartesian to javafx.fxml;
+    exports com.gmrossetti.mdp.entity.waypoint;
+    opens com.gmrossetti.mdp.entity.waypoint to javafx.fxml;
 }
