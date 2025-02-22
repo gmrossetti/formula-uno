cb8269b5140999e991ed06f0024c16d64f75f3d0
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-22 17:06:36 +0100

    Update | WaypointView

--- src/main/java/com/gmrossetti/mdp/view/WaypointView.java
+++ src/main/java/com/gmrossetti/mdp/view/WaypointView.java
@@ -1,6 +1,7 @@
 package com.gmrossetti.mdp.view;
 
 import com.gmrossetti.mdp.entity.Waypoint;
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -18,6 +19,12 @@ public class WaypointView extends Pane {
 
         circle.setStrokeWidth(1.5);
 
-        this.getChildren().add(circle);
+        Circle center = new Circle(SPACING * waypoint.getCenter().x, SPACING * waypoint.getCenter().y,
+                SPACING * 0.3);
+        center.setFill(Color.web("#FFaa00"));
+
+        Group group = new Group(circle, center);
+
+        this.getChildren().add(group);
     }
 }
