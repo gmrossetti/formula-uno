7ad0d55db63eac9ba07f5361f7e1d0f5035a3e29
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-23 16:41:08 +0100

    Add | advanced AI BotStrategy

--- src/main/java/com/gmrossetti/mdp/entity/cartesian/ProperGridLine.java
+++ src/main/java/com/gmrossetti/mdp/entity/cartesian/ProperGridLine.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public interface ProperGridLine {
     double getSlopeCoefficient();
+    double getSlopeCoefficientToDegrees();
     Set<Point> getLineIntersectionsWithGrid();
     List<GridPoint> getNearestGridPointsOnIntersections();
 }
