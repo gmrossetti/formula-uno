b911d7e20f323618424350c31ceaab18cb7e3648
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-02-23 23:01:53 +0100

    Refactor

--- src/main/java/com/gmrossetti/mdp/driver/StrategyParameters.java
+++ src/main/java/com/gmrossetti/mdp/driver/StrategyParameters.java
@@ -6,21 +6,6 @@ public class StrategyParameters {
     private final double brakeDistance;
     private final double accelerateDistance;
 
-    // TODO: dynamic load from JSON
-//    public StrategyParameters(){
-//        this.minVelocity = 0.5;
-//        this.deviationThreshold = 0.5;
-//        this.brakeDistance = 0.5;
-//        this.accelerateDistance = 0.5;
-//    }
-
-//    public StrategyParameters(){
-//        this.minVelocity = 0.7;
-//        this.deviationThreshold = 0.7;
-//        this.brakeDistance = 0.4;
-//        this.accelerateDistance = 0.6;
-//    }
-
     public StrategyParameters(double minVelocity, double deviationThreshold, double brakeDistance, double accelerateDistance) {
         this.minVelocity = minVelocity;
         this.deviationThreshold = deviationThreshold;
