fd8d1327eac60b33a91c1c994ff852be73a2cd03
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-04-06 23:19:57 +0200

    Update | game config file parsing system

--- src/main/java/com/gmrossetti/mdp/strategy/StrategyType.java
+++ src/main/java/com/gmrossetti/mdp/strategy/StrategyType.java
@@ -9,8 +9,16 @@ public enum StrategyType {
     StrategyType(String value) {
         this.value = value;
     }
-
     public String getValue() {
         return value;
+    }
+    public static StrategyType fromValue(String value) {
+        for (StrategyType strategy : StrategyType.values()) {
+            if (strategy.getValue().equals(value)) {
+                return strategy;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown StrategyType value: " + value);
     }
 }
