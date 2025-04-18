6bea5a7643777aefa24bf0ae02cd5c88394177ed
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-04-18 19:56:37 +0200

    Docs | IStrategy & StrategyFactory

--- src/main/java/com/gmrossetti/mdp/formulauno/strategy/IStrategy.java
+++ src/main/java/com/gmrossetti/mdp/formulauno/strategy/IStrategy.java
@@ -4,6 +4,17 @@ import com.gmrossetti.mdp.formulauno.circuit.ICircuit;
 import com.gmrossetti.mdp.formulauno.driver.IDriver;
 import com.gmrossetti.mdp.formulauno.driver.move.Move;
 
+/**
+ * Interface representing a strategy for choosing the best move for a driver on a circuit.
+ * Implementations of this interface should provide specific strategies for move selection.
+ */
 public interface IStrategy {
+    /**
+     * Chooses the best move for the given driver on the specified circuit.
+     *
+     * @param driver  The driver for whom to choose the best move.
+     * @param circuit The circuit on which the driver is racing.
+     * @return The best move for the driver on the circuit.
+     */
     Move chooseBestMove(IDriver driver, ICircuit circuit);
 }
