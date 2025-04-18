ecaae48ff3ce56c857088bad24a5cea2d157b97e
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-04-18 18:44:36 +0200

    Docs | pawn package

--- src/main/java/com/gmrossetti/mdp/formulauno/pawn/PawnFactory.java
+++ src/main/java/com/gmrossetti/mdp/formulauno/pawn/PawnFactory.java
@@ -3,6 +3,10 @@ package com.gmrossetti.mdp.formulauno.pawn;
 import com.gmrossetti.mdp.formulauno.cartesian.GridPoint;
 
 // TODO: add type and support for ipotetical different pawns
+/**
+ * Factory class for creating instances of IPawn.
+ * Currently, it only creates instances of Car.
+ */
 public final class PawnFactory {
     public static IPawn buildPawn(GridPoint initialPosition){
         return new Car(initialPosition);
