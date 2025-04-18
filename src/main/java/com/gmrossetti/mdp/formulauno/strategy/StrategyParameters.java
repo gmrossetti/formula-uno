0f26aaaade47c927b516873fd5333b0f1ed55221
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-04-18 20:05:56 +0200

    Add | validation constructor StrategyParameters

--- src/main/java/com/gmrossetti/mdp/formulauno/strategy/StrategyParameters.java
+++ src/main/java/com/gmrossetti/mdp/formulauno/strategy/StrategyParameters.java
@@ -1,3 +1,12 @@
 package com.gmrossetti.mdp.formulauno.strategy;
 
-public record StrategyParameters(double minVelocity, double deviationThreshold, double brakeDistance, double accelerateDistance) {}
+public record StrategyParameters(double minVelocity, double deviationThreshold, double brakeDistance, double accelerateDistance) {
+    public StrategyParameters {
+        if (minVelocity < 0 || minVelocity > 1 ||
+                deviationThreshold < 0 || deviationThreshold > 1 ||
+                brakeDistance < 0 || brakeDistance > 1 ||
+                accelerateDistance < 0 || accelerateDistance > 1) {
+            throw new IllegalArgumentException("All normalized values must be between 0 and 1");
+        }
+    }
+}
