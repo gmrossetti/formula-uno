c33f0131effb58d615b7413b43e0fa76119012a0
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-04-18 18:28:51 +0200

    Docs | leaderboard package

--- src/main/java/com/gmrossetti/mdp/formulauno/leaderboard/LeaderboardEntry.java
+++ src/main/java/com/gmrossetti/mdp/formulauno/leaderboard/LeaderboardEntry.java
@@ -2,4 +2,11 @@ package com.gmrossetti.mdp.formulauno.leaderboard;
 
 import com.gmrossetti.mdp.formulauno.driver.IDriver;
 
+/**
+ * Represents an entry in the leaderboard.
+ *
+ * @param driver          The driver associated with this entry.
+ * @param isDisqualified   Indicates if the driver is disqualified.
+ * @param annotations      Additional annotations or comments about the entry.
+ */
 public record LeaderboardEntry(IDriver driver, boolean isDisqualified, String annotations) {}
