8510dd6796cf739ef618c34c5df1fa10cdae566a
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-04-18 17:39:35 +0200

    Docs | IDriver interface

--- src/main/java/com/gmrossetti/mdp/formulauno/driver/IDriver.java
+++ src/main/java/com/gmrossetti/mdp/formulauno/driver/IDriver.java
@@ -8,10 +8,44 @@ import com.gmrossetti.mdp.formulauno.pawn.IPawn;
 
 import java.util.Map;
 
+/**
+ * The IDriver interface represents a driver in the Formula Uno game.
+ * It provides methods to get the car, check for active waypoints,
+ * retrieve the target waypoint, make a move, and get move points.
+ */
 public interface IDriver {
+    /**
+     * Gets the car associated with this driver.
+     *
+     * @return The car of the driver.
+     */
     IPawn getCar();
+
+    /**
+     * Checks if the driver has an active waypoint.
+     *
+     * @return True if there is an active waypoint, false otherwise.
+     */
     boolean hasActiveWaypoint();
+
+    /**
+     * Gets the target waypoint for the driver.
+     *
+     * @return The target waypoint.
+     */
     Waypoint getWaypointTarget();
+
+    /**
+     * Makes a move for the driver.
+     *
+     * @return The grid line representing the move.
+     */
     GridLine makeMove();
+
+    /**
+     * Gets the points associated with the moves.
+     *
+     * @return A map of moves to their corresponding grid points.
+     */
     Map<Move, GridPoint> getMovesPoints();
 }
