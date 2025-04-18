a978e1c1a7abb00ddcad86752ccfbced36616230
gmrossetti <47495571+gmrossetti@users.noreply.github.com>
2025-04-18 17:14:56 +0200

    Docs | Tile package

--- src/main/java/com/gmrossetti/mdp/formulauno/circuit/tile/ITile.java
+++ src/main/java/com/gmrossetti/mdp/formulauno/circuit/tile/ITile.java
@@ -3,10 +3,42 @@ package com.gmrossetti.mdp.formulauno.circuit.tile;
 import com.gmrossetti.mdp.formulauno.cartesian.GridPoint;
 import com.gmrossetti.mdp.formulauno.pawn.IPawn;
 
+/**
+ * Interface representing a tile on the circuit.
+ * A tile can be occupied by a pawn and has a specific grid point.
+ */
 public interface ITile {
+    /**
+     * Gets the grid point of the tile.
+     *
+     * @return the grid point of the tile
+     */
     GridPoint getGridPoint();
+    /**
+     * Checks if the tile is on track.
+     *
+     * @return true if the tile is on track, false otherwise
+     */
     boolean isOnTrack();
+
+    /**
+     * Gets the pawn occupying the tile.
+     *
+     * @return IPawn object representing the pawn occupying the tile, null if not occupied
+     */
     IPawn getOccupiedBy();
+
+    /**
+     * Checks if the tile is occupied by another player/Pawn.
+     *
+     * @return true if the tile is occupied, false otherwise
+     */
     boolean isOccupied();
+
+    /**
+     * Sets the tile to be occupied by a pawn.
+     *
+     * @param occupiedBy the pawn occupying the tile
+     */
     void setOccupiedBy(IPawn occupiedBy);
 }
--- src/main/java/com/gmrossetti/mdp/formulauno/circuit/tile/Tile.java
+++ src/main/java/com/gmrossetti/mdp/formulauno/circuit/tile/Tile.java
@@ -3,16 +3,43 @@ package com.gmrossetti.mdp.formulauno.circuit.tile;
 import com.gmrossetti.mdp.formulauno.cartesian.GridPoint;
 import com.gmrossetti.mdp.formulauno.pawn.IPawn;
 
+/**
+ * Class representing a tile on the circuit.
+ * A tile can be occupied by a pawn and has a specific grid point.
+ */
 public class Tile implements ITile {
+    /**
+     * The grid point of the tile.
+     */
     private final GridPoint gridPoint;
+    /**
+     * Indicates if the tile is on track.
+     */
     private final boolean isOnTrack;
+    /**
+     * The pawn occupying the tile.
+     */
     private IPawn occupiedBy;
 
+    /**
+     * Constructor for creating a tile with a grid point and track status.
+     *
+     * @param gridPoint the grid point of the tile
+     * @param isOnTrack true if the tile is on track, false otherwise
+     */
     public Tile(GridPoint gridPoint, boolean isOnTrack) {
         this.gridPoint = gridPoint;
         this.isOnTrack = isOnTrack;
         this.occupiedBy = null;
     }
+
+    /**
+     * Constructor for creating a tile with a grid point, track status, and an occupying pawn.
+     *
+     * @param gridPoint the grid point of the tile
+     * @param isOnTrack true if the tile is on track, false otherwise
+     * @param occupiedBy the pawn occupying the tile
+     */
     public Tile(GridPoint gridPoint, boolean isOnTrack, IPawn occupiedBy) {
         this.gridPoint = gridPoint;
         this.isOnTrack = isOnTrack;
